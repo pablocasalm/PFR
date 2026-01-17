cbb56932cbb39b77b3d96e60c288185257243177
pablocasalm <pablo_casal@icloud.com>
2026-01-17 11:22:45 +0100

    Eliminado archivo zip

--- PadelFilmRoom_zip/PadelFilmRoom/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"i28zFaRQ5eSzkZFbanJaIyxDCXOFB9tPnET18sAAGgo=\"\n}"
--- PadelFilmRoom_zip/PadelFilmRoom/.next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__82d5522d._.css",
-  "static/chunks/node_modules_a22d5b18._.js",
-  "static/chunks/_99106acc._.js"
-],
-    source: "dynamic"
-});
